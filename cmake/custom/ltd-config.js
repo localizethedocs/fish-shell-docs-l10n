d5fe56a4d91ddc5ce1eae615a4f71ac41a471bb9
Haowei Hsu <hwhsu1231@localizethedocs.org>
2026-03-11 11:36:49 +0800

    ✨ feat(init): initialize branch 'main' (GH-1)
    Initialize the 'main' branch with the following files:
    
    - Add document files: README.md, LICENSE-BSD, LICENSE-CC.
    - Add GitHub workflow files: ci-check-pr-status,..., etc.
    - Add GitHub issue templates: language_request.yml,..., etc.
    - Add Git config files: .gitmodules, .gitignore.
    - Add Git submodules: cmake/modules, cmake/plugins.
    - Add CMake script files: CMakeLists.txt, cmake/**/* files.
    - Add config files: versions.json, languages.json, crowdin.yml.

---
+++ cmake/custom/ltd-config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["current", "Current Release"],
+    ["4",       "Release 4.x"],
+    ["3",       "Release 3.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/fish-shell-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/fish-shell-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/fish-shell-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/fish-shell-docs-l10n"],
+  ]
+};
