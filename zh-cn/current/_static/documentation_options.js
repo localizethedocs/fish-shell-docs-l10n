f596d8961f7aac408169bf6ef7c1232d0b6dbc0a
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-21 16:29:51 +0000

    Deploying to pages from @ localizethedocs/fish-shell-docs-l10n@162783db7d4a3df70fe4a93571114bf1197863e4 🚀

---
+++ zh-cn/current/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.9.3-g5bd98b9',
+    LANGUAGE: 'zh-CN',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
